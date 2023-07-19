a0ce1d8b8a6f4314927b4312edb14a6c3a950049
Milan <milan.letic@gmail.com>
2023-07-19 19:30:39 +0200

    Add project files.

---
+++ SqlFormatter/Model/Common.cs
@@ -0,0 +1,16 @@
+﻿namespace SqlFormatter.Model;
+
+public class Statistics
+{
+    public int ElapsedMicroseconds { get; set; }
+    public int TotalLinesOfCode { get; set; }
+    public int NonEmptyLinesOfCode { get; set; }
+}
+
+public class Value
+{
+    public bool Success { get; set; }
+    public string ErrorMessage { get; set; }
+    public List<Token> Tokens { get; set; }
+    public Statistics Statistics { get; set; }
+}
---
+++ SqlFormatter/Model/Token.cs
@@ -0,0 +1,39 @@
+﻿namespace SqlFormatter.Model;
+
+public enum TokenType
+{
+    Literal,
+    Whitespace,
+    ReservedKeyword,
+    Semicolon,
+    Operator,
+    Identifier,
+    Comment,
+    Comma,
+    OpenParen,
+    CloseParen,
+}
+
+public class Token
+{
+    public string Content { get; set; }
+
+    public TokenType Type { get; set; }
+    public int Start { get; set; }
+    public int End { get; set; }
+    public int Length => End - Start;
+}
+
+public class TokenValue : Value
+{
+    public List<Token> Tokens { get; set; }
+}
+
+public class TokenList
+{
+    public TokenValue Value { get; set; }
+    public List<object> Formatters { get; set; }
+    public List<object> ContentTypes { get; set; }
+    public object DeclaredType { get; set; }
+    public int StatusCode { get; set; }
+}
